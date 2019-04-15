4ba60871127e6a1186a9e04c2d6cf0b2e37a9970
Lanisya <goodgirl7299@gmail.com>
2019-04-16 00:22:56 +0700

    first commit

---
+++ app/src/main/java/com/example/lenovo/breakoutgame/Paddle.java
@@ -0,0 +1,71 @@
+package com.example.lenovo.breakoutgame;
+
+import android.graphics.RectF;
+
+public class Paddle {
+
+
+    private RectF rect;
+
+    private float length;
+    private float height;
+
+    private float x;
+    private float y;
+
+    private int screenX;
+    private int screenY;
+
+    private float paddleSpeed;
+
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    private int paddleMoving = STOPPED;
+
+    public Paddle(int screenX, int screenY){
+
+        length = 130;
+        height = 20;
+
+        this.screenX = screenX;
+        this.screenY = screenY;
+
+        x = (screenX / 2) - (length / 2);
+        y = screenY - 10;
+
+        rect = new RectF(x, y, x + length, y + height);
+
+        paddleSpeed = 350;
+    }
+
+    public RectF getRect(){
+        return rect;
+    }
+
+    public void setMovementState(int state){
+        paddleMoving = state;
+    }
+
+    public void update(long fps){
+        if(paddleMoving == LEFT && x > 0){
+            x = x - paddleSpeed / fps;
+        }
+
+        if(paddleMoving == RIGHT && x < screenX - length){
+            x = x + paddleSpeed / fps;
+        }
+
+        rect.left = x;
+        rect.right = x + length;
+    }
+
+    public void reset(int x, int y) {
+        rect.left = x /2 - length / 2;
+        rect.right = x /2 + length /2;
+        this.x = (screenX / 2) - (length / 2);
+        this.y = screenY - 20;
+    }
+}
+
